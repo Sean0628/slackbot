cfd6b49f154e3a1615272520ff5f70b0a524a5f6
Sho Ito <i.sho.0628@gmail.com>
2018-07-01 15:21:36 +0900

    define several methods and devide functions

--- helper.rb
+++ helper.rb
@@ -2,4 +2,8 @@ module Helper
   def format_date(date)
     date.strftime('%m月%d日')
   end
+
+  def to_unix_ts(date)
+    date.to_time.to_i
+  end
 end
--- lunchbot.rb
+++ lunchbot.rb
@@ -1,6 +1,7 @@
 require 'dotenv'
 require 'slack'
 require 'date'
+require 'time'
 require './helper.rb'
 
 class LunchBot
@@ -9,25 +10,25 @@ class LunchBot
 
   TARGET_CHANNEL = ENV['TARGET_CHANNEL']
   BOT_ID = ENV['BOT_ID']
-  BOT_TOKEN = ENV['BOT_TOKEN']
+  OAUTH_TOKEN = ENV['OAUTH_TOKEN']
+  USER_NAME = '勝手にランチBot'.freeze
 
   def initialize
     Slack.configure do |config|
-      config.token = BOT_TOKEN
+      config.token = OAUTH_TOKEN
     end
   end
 
   def run
     client = Slack::Client.new
-    channel_members = client.channels_info(channel: TARGET_CHANNEL)['channel']['members']
-                              .reject! { |id| id == BOT_ID }
-                              .map { |id| "<@#{id}>さん" }
-    selected_members = channel_members.sample(2).join(' ')
-    client.chat_postMessage(channel: TARGET_CHANNEL, text: generate_text(selected_members), as_user: true)
+    message_within1week =
+      extract_messages_within1week(get_channel_history(client))
+    unavailable_user = extract_unavailable_user(message_within1week) << BOT_ID
+    pick_members(client, unavailable_user)
+    post_message(client)
   end
 
-  private
-
+ private
   def generate_text(members)
     next_tuesday = Date.today + 7
     <<~"EOS"
@@ -36,6 +37,44 @@ class LunchBot
     #{members}
     一緒にランチに行ってらっしゃい :meat_on_bone: :green_salad: :cake:
     EOS
+  end
+
+  def post_message(client)
+    client.chat_postMessage(channel: TARGET_CHANNEL,
+                            text: generate_text(pick_members(client)),
+                            as_user: false,
+                            username: USER_NAME)
+  end
+
+  def get_channel_history(client)
+    a_week_ago = Date.today - 7
+    client.channels_history(channel: TARGET_CHANNEL,
+                            oldest: to_unix_ts(a_week_ago))['messages']
+  end
+
+  def extract_messages_within1week(history)
+    messages_within1week = []
+    history.each do |record|
+      messages_within1week << record.select { |k| k == 'user' || k == 'text' }
+    end
+    messages_within1week
+  end
+
+  def extract_unavailable_user(messages)
+    denials = %w[パス pass ぱす]
+    unavailable_user = []
+    messages.each do |message|
+      unavailable_user << message['user'] if denials.include?(message['text'])
+    end
+    unavailable_user
+  end
+
+  def pick_members(client, user)
+    channel_members =
+      client.channels_info(channel: TARGET_CHANNEL)['channel']['members']
+            .reject { |id| user.include?(id) }
+            .map { |id| "<@#{id}>さん" }
+    channel_members.sample(4).join(' ')
   end
 end
 
