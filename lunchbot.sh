a2ae3c32bc49967627d433d739774f3be62cf5b7
Sho Ito <i.sho.0628@gmail.com>
2018-07-28 21:12:39 +0900

    add shell script

---
+++ lunchbot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+readonly THURSDAY=4
+
+if [ $(date +%u) != $THURSDAY ]; then
+  exit 1
+fi
+
+month=$(date +%m)
+next_week_month=$(date -v +7d +%m)
+if [ $month != $next_week_month ]; then
+  exit 1
+fi
+
+ruby ./lib/lunchbot.rb
+exit 0
