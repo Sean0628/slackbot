34ef210cd1ac39788a060f75327b5bc59b19c071
Sho Ito <i.sho.0628@gmail.com>
2018-08-19 17:01:07 +0900

    remove logic not to activate this app on the last thursday of the month

--- lunchbot.sh
+++ lunchbot.sh
@@ -6,11 +6,5 @@ if [ $(date +%u) != $THURSDAY ]; then
   exit 1
 fi
 
-month=$(date +%m)
-next_week_month=$(date +%m --date '7 days')
-if [ $month != $next_week_month ]; then
-  exit 1
-fi
-
 ruby ./lib/lunchbot.rb
 exit 0
